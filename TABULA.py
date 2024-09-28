77f37397e73097773363fe3d1e88dacee1b5ae80
TheWorldiMap <46360786+TheWorldiMap@users.noreply.github.com>
2024-09-28 15:10:29 +1000

    Prep for GUI overhaul and load function

--- TABULA.py
+++ TABULA.py
@@ -26,108 +26,115 @@ from tkinter import ttk
 import tkinter.dialog
 import tkinter.filedialog
  
-class GLOBAL_DATA:
+class GLOBAL_VARS: ## Data that everything touches 
     
     GameEntriesList = [] ## part of the next system to have seperated folders for each game       
     SourceEntriesList = [] ## list as displayed in the GOOEY (this is messy af and needs to be STOPPED)
     EntriesList = []  ## Workhorse Data stucture should be an easy JSON when i lean that
     FormattedEntries = [] ## workhorse part 2
+    BackedUpFiles = [] ## list path to files fully backed up for listing in loading
+    BackedUpPaths = [] ## list paths created to store files for listing  in loading
 
     Today = datetime.today().strftime('%Y-%m-%d')
 
-class LIST_MANAGER:
+class LIST_MANAGER: ## Manages all the Lists functions
+           
+    def FormatEntries():
+        list_games = [Game["Game"] for Game in GLOBAL_VARS.EntriesList]
+        list_source_files = [SourceFile["Source File"] for SourceFile in GLOBAL_VARS.EntriesList]
+        list_source_paths = [SourcePath["Source Path"] for SourcePath in GLOBAL_VARS.EntriesList]
+        list_destination_paths = [DestinationPath["Destination Path"] for DestinationPath in GLOBAL_VARS.EntriesList]
+
+        for Game, SourceFile, SourcePath, DestinationPath in zip(list_games, list_source_files, list_source_paths, list_destination_paths):
+            full_source = (SourcePath + "\\" + SourceFile)
+            full_destination = (DestinationPath + "\\" + Game + "\\" + GLOBAL_VARS.Today + "\\" + SourceFile)
+            destination_path = (DestinationPath + "\\" + Game + "\\" + GLOBAL_VARS.Today + "\\")
     
-        def BackupList(): ## this looks kinda raw to my eyes but i am actually proud of how simple this is
-            LIST_MANAGER.FormatEntries()
-            list_full_sources = [FullSource["Full Source"] for FullSource in GLOBAL_DATA.FormattedEntries]
-            list_full_destinations = [FullDestination["Full Destination"] for FullDestination in GLOBAL_DATA.FormattedEntries]
-            list_destination_paths = [DestinationPath["Destination Path"] for DestinationPath in GLOBAL_DATA.FormattedEntries]
+            formatted_entry = {
+        "Full Source" : full_source,
+        "Full Destination" : full_destination,
+        "Destination Path" : destination_path
+        }
+            GLOBAL_VARS.FormattedEntries.append(formatted_entry)
     
-            for FullDestination, FullSource, DestinationPath in zip(list_full_destinations, list_full_sources, list_destination_paths):
-                if not os.path.exists(DestinationPath):
-                    os.makedirs(DestinationPath)
-                    shutil.copy(FullSource, FullDestination)
-                else:
-                    shutil.copy(FullSource, FullDestination)
+    def SaveToList(self): ## i could do this better if i knew what i was doing when i started it... -- fixed it a bit but also MAKE THIS A CLASS 
+        game_entered = TABULA_GUI.GameEntered.get()
+        source_entered = TABULA_GUI.SourcePathEntered.get()
+        destination_entered = TABULA_GUI.DestinationPathEntered.get() 
+        source_file = os.path.basename(source_entered)
+        source_path = os.path.dirname(source_entered)
+        entires_dict = {
+            "Game" : game_entered,
+            "Source File" : source_file,
+            "Source Path" : source_path, 
+            "Destination Path" : destination_entered,
+        }
+        GLOBAL_VARS.EntriesList.append(entires_dict)
+        GLOBAL_VARS.SourceEntriesList.append(source_entered)
+        GLOBAL_VARS.GameEntriesList.append(game_entered)
+        self.UpdateLists()
+
+    def UpdateLists(self):
+        TABULA_GUI.GameNameListVar.set(GLOBAL_VARS.GameEntriesList)
+        TABULA_GUI.SaveFileListVar.set(GLOBAL_VARS.SourceEntriesList)
+
+    def EnableListButton(*args):
+        TABULA_GUI.RemoveListButton.configure(state=ACTIVE)
+    def RemoveFromList(): ## this is awful and im sure there is a nicer way to do it BUT IT JUST WORKS
+        SelectedIndex = TABULA_GUI.SaveListBox.curselection()
+        list(SelectedIndex)
+        del GLOBAL_VARS.SourceEntriesList[SelectedIndex[0]]
+        del GLOBAL_VARS.GameEntriesList[SelectedIndex[0]]
+        TABULA_GUI.SaveListBox.delete([SelectedIndex[0]])
+        TABULA_GUI.GameNameBox.delete([SelectedIndex[0]])
+        GLOBAL_VARS.EntriesList.pop(SelectedIndex[0])
+        TABULA_GUI.RemoveListButton.configure(state=DISABLED)
+        LIST_MANAGER.UpdateLists()
+
+    def SourceFileEntryBrowse():
+        source_path_entered = tkinter.filedialog.askopenfilename()
+        TABULA_GUI.SourcePathEntry.delete(0, END)
+        TABULA_GUI.SourcePathEntry.insert(1, source_path_entered)
+
+    def DestinationPathEntryBrowse():
+        destination_path_entered = tkinter.filedialog.askdirectory()
+        TABULA_GUI.DestinationPathEntry.delete(0, END)
+        TABULA_GUI.DestinationPathEntry.insert(1, destination_path_entered)
+
+class BACKUP_AND_LOAD:
             
-        def FormatEntries():
-            list_games = [Game["Game"] for Game in GLOBAL_DATA.EntriesList]
-            list_source_files = [SourceFile["Source File"] for SourceFile in GLOBAL_DATA.EntriesList]
-            list_source_paths = [SourcePath["Source Path"] for SourcePath in GLOBAL_DATA.EntriesList]
-            list_destination_paths = [DestinationPath["Destination Path"] for DestinationPath in GLOBAL_DATA.EntriesList]
-    
-            for Game, SourceFile, SourcePath, DestinationPath in zip(list_games, list_source_files, list_source_paths, list_destination_paths):
-                full_source = (SourcePath + "\\" + SourceFile)
-                full_destination = (DestinationPath + "\\" + Game + "\\" + GLOBAL_DATA.Today + "\\" + SourceFile)
-                destination_path = (DestinationPath + "\\" + Game + "\\" + GLOBAL_DATA.Today + "\\")
+    def BackupSaves(): ## this looks kinda raw to my eyes but i am actually proud of how simple this is
+        LIST_MANAGER.FormatEntries()
+        list_full_sources = [FullSource["Full Source"] for FullSource in GLOBAL_VARS.FormattedEntries]
+        list_full_destinations = [FullDestination["Full Destination"] for FullDestination in GLOBAL_VARS.FormattedEntries]
+        list_destination_paths = [DestinationPath["Destination Path"] for DestinationPath in GLOBAL_VARS.FormattedEntries]
         
-                formatted_entry = {
-            "Full Source" : full_source,
-            "Full Destination" : full_destination,
-            "Destination Path" : destination_path
-            }
-                GLOBAL_DATA.FormattedEntries.append(formatted_entry)
-        
-        def SaveToList(self): ## i could do this better if i knew what i was doing when i started it... -- fixed it a bit but also MAKE THIS A CLASS 
-            game_entered = TABULA_GUI.GameEntered.get()
-            source_entered = TABULA_GUI.SourcePathEntered.get()
-            destination_entered = TABULA_GUI.DestinationPathEntered.get() 
-            source_file = os.path.basename(source_entered)
-            source_path = os.path.dirname(source_entered)
-            entires_dict = {
-                "Game" : game_entered,
-                "Source File" : source_file,
-                "Source Path" : source_path, 
-                "Destination Path" : destination_entered,
-            }
-            GLOBAL_DATA.EntriesList.append(entires_dict)
-            GLOBAL_DATA.SourceEntriesList.append(source_entered)
-            GLOBAL_DATA.GameEntriesList.append(game_entered)
-            self.UpdateLists()
+        for FullDestination, FullSource, DestinationPath in zip(list_full_destinations, list_full_sources, list_destination_paths):
+            if not os.path.exists(DestinationPath):
+                os.makedirs(DestinationPath)
+                shutil.copy(FullSource, FullDestination)
+                GLOBAL_VARS.BackedUpFiles.append(FullDestination)
+                GLOBAL_VARS.BackedUpPaths.append(DestinationPath)
+            else:
+                shutil.copy(FullSource, FullDestination)
+                GLOBAL_VARS.BackedUpFiles.append(FullDestination)
+                GLOBAL_VARS.BackedUpPaths.append(DestinationPath)
     
-        def UpdateLists(self):
-            TABULA_GUI.GameNameListVar.set(GLOBAL_DATA.GameEntriesList)
-            TABULA_GUI.SaveFileListVar.set(GLOBAL_DATA.SourceEntriesList)
-    
-        def EnableListButton(*args):
-            TABULA_GUI.RemoveListButton.configure(state=ACTIVE)
-
-        def RemoveFromList(): ## this is awful and im sure there is a nicer way to do it BUT IT JUST WORKS
-            SelectedIndex = TABULA_GUI.SaveListBox.curselection()
-            list(SelectedIndex)
-            del GLOBAL_DATA.SourceEntriesList[SelectedIndex[0]]
-            del GLOBAL_DATA.GameEntriesList[SelectedIndex[0]]
-            TABULA_GUI.SaveListBox.delete([SelectedIndex[0]])
-            TABULA_GUI.GameNameBox.delete([SelectedIndex[0]])
-            GLOBAL_DATA.EntriesList.pop(SelectedIndex[0])
-            TABULA_GUI.RemoveListButton.configure(state=DISABLED)
-            LIST_MANAGER.UpdateLists()
-    
-        def SourceFileEntryBrowse():
-            source_path_entered = tkinter.filedialog.askopenfilename()
-            TABULA_GUI.SourcePathEntry.delete(0, END)
-            TABULA_GUI.SourcePathEntry.insert(1, source_path_entered)
-    
-        def DestinationPathEntryBrowse():
-            destination_path_entered = tkinter.filedialog.askdirectory()
-            TABULA_GUI.DestinationPathEntry.delete(0, END)
-            TABULA_GUI.DestinationPathEntry.insert(1, destination_path_entered)
-
-class DATA_SAVER:
+class DATA_SAVER: ## Saves Backup lists and other things to a dat
     
     def SaveList():
         with open("BackupsList.dat", "wb") as Data:
-            pickle.dump(GLOBAL_DATA.EntriesList, Data)
-            pickle.dump(GLOBAL_DATA.SourceEntriesList, Data)
-            pickle.dump(GLOBAL_DATA.GameEntriesList, Data)
-            pickle.dump(GLOBAL_DATA.FormattedEntries, Data)
+            pickle.dump(GLOBAL_VARS.EntriesList, Data)
+            pickle.dump(GLOBAL_VARS.SourceEntriesList, Data)
+            pickle.dump(GLOBAL_VARS.GameEntriesList, Data)
+            pickle.dump(GLOBAL_VARS.FormattedEntries, Data)
         
     def LoadList():
             with open("BackupsList.dat", "rb") as Data:
-                GLOBAL_DATA.EntriesList = pickle.load(Data)
-                GLOBAL_DATA.SourceEntriesList = pickle.load(Data)
-                GLOBAL_DATA.GameEntriesList = pickle.load(Data)
-                GLOBAL_DATA.FormattedEntries = pickle.load(Data)    
+                GLOBAL_VARS.EntriesList = pickle.load(Data)
+                GLOBAL_VARS.SourceEntriesList = pickle.load(Data)
+                GLOBAL_VARS.GameEntriesList = pickle.load(Data)
+                GLOBAL_VARS.FormattedEntries = pickle.load(Data)    
                    
     def AutoLoad():
         if os.path.exists("BackupsList.dat"):
@@ -148,8 +155,8 @@ class TABULA_GUI: ## GOOEY - this is kinda wackily formatted and will be a probl
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        SaveFileListVar = StringVar(value=GLOBAL_DATA.SourceEntriesList)
-        GameNameListVar = StringVar(value=GLOBAL_DATA.GameEntriesList)
+        SaveFileListVar = StringVar(value=GLOBAL_VARS.SourceEntriesList)
+        GameNameListVar = StringVar(value=GLOBAL_VARS.GameEntriesList)
 
         ttk.Label(mainframe, text="TWIM's semi-Automatic Back-Up & Load Application").grid(column=2, row=1, sticky=(N))
 
@@ -173,7 +180,8 @@ class TABULA_GUI: ## GOOEY - this is kinda wackily formatted and will be a probl
         ttk.Button(mainframe, text="Browse", command=LIST_MANAGER.DestinationPathEntryBrowse).grid(column=2, row=18, sticky=(E), pady=3)
 
         ttk.Button(mainframe, text="Add Save", command=LIST_MANAGER.SaveToList).grid(column=2, row=20, sticky=(S, W), pady=(3, 5))
-
+        
+        ### MAKE THIS A TREEVIEW MY GOD 
         SaveListBox = Listbox(mainframe, listvariable=SaveFileListVar, height=12, width=120)
         SaveListBox.grid(column=2, row=21, sticky=(W))
         GameNameBox = Listbox(mainframe, listvariable=GameNameListVar, height=12, width=15, state=DISABLED)
@@ -181,10 +189,11 @@ class TABULA_GUI: ## GOOEY - this is kinda wackily formatted and will be a probl
 
         RemoveListButton = ttk.Button(mainframe, text="Remove Save", state=DISABLED, command=LIST_MANAGER.RemoveFromList)
         RemoveListButton.grid(column=2, row=20, sticky=(S, E), pady=(0, 5))
+        ## SERIOUSLY DO IT DUMMY
 
         SaveListBox.bind("<Button-1>", LIST_MANAGER.EnableListButton)
 
-        ttk.Button(mainframe, text="Backup List", command=LIST_MANAGER.BackupList).grid(column=2, row=27, sticky=(S), pady=2)
+        ttk.Button(mainframe, text="Backup List", command=BACKUP_AND_LOAD.BackupSaves).grid(column=2, row=27, sticky=(S), pady=2)
 
         ttk.Button(mainframe, text="Save List", command=DATA_SAVER.SaveList).grid(column=2, row=27, sticky=(S, W), pady=2)
 
